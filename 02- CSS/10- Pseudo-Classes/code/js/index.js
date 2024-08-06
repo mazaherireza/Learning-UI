7e01af45aa1eb71953fce3c3ad84caac739259b3
mazaherireza <rezamazaheri.email@gmail.com>
2024-08-06 19:11:59 +0330

    CSS: 10- Edition

---
+++ 02- CSS/10- Pseudo-Classes/code/js/index.js
@@ -0,0 +1,23 @@
+const $ = document;
+const form = $.querySelector("form");
+const programmingLanguages = ["C", "PHP", "Python", "Golang"];
+
+let content = "";
+const populateContent = () => {
+  programmingLanguages.forEach((programmingLanguage) => {
+    const lowercase = programmingLanguage.toLowerCase();
+    const html = `
+        <input type="checkbox" id=${lowercase} name=${programmingLanguage} value=${programmingLanguage} />
+        <label for=${lowercase}>
+          <span>${programmingLanguage}</span> 
+          <span><i class="fa fa-heart"></i></span>
+        </label>
+        `;
+    content += html;
+  });
+};
+
+window.onload = () => {
+  populateContent();
+  form.insertAdjacentHTML("beforeend", content);
+};
