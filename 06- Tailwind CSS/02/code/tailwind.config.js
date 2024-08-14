3bdeca1d11570b08bfef11204446a37df7ce8728
mazaherireza <rezamazaheri.email@gmail.com>
2024-08-14 12:10:21 +0330

    Tailwind CSS: 02

---
+++ 06- Tailwind CSS/02/code/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["sans-serif"],
+      serif: ["Times New Roman", "serif"],
+      inter: ["Inter"]
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "",
+          100: "",
+          200: "",
+          300: "",
+          400: "#7f00ff",
+          500: "",
+          600: "",
+          700: "",
+          800: "",
+          900: "",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
