149ac4581a88a7043ca7c2a2f88d346ba7554f04
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-26 11:57:58 +0330

    CSS Grid: 19 - Refactor

--- 04- CSS Grid/19- Exibition/code/scripts/index.js
+++ 04- CSS Grid/19- Exibition/code/scripts/index.js
@@ -1,18 +1,17 @@
 const $ = document;
-const wrapper = $.querySelector(".wrapper");
+const main = $.querySelector("main");
 const LIMIT = 8;
 
 const renderToDOM = () => {
   const html = `
     <div class="product-card">
-      <div class="img-wrapper">
-        <img src="./assets/images/Perspolis.png" alt="Perspolis" />
-      </div>
+      <img src="/assets/images/perspolis.png" alt="" />
       <button>Buy</button>
     </div>
   `;
+
   for (let index = 0; index < LIMIT; index++) {
-    wrapper.insertAdjacentHTML("beforeend", html);
+    main.insertAdjacentHTML("beforeend", html);
   }
 };
 
